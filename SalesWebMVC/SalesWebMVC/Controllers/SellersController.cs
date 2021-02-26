b1f5c75825d1615c617639dca0d5643ac488e057
RacconWebFenix <dfenixweb@gmail.com>
2021-02-26 08:15:22 -0300

    Delete seller

--- SalesWebMVC/SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/SalesWebMVC/Controllers/SellersController.cs
@@ -14,11 +14,12 @@ namespace SalesWebMVC.Controllers
         private readonly SellerService _sellerService;
         private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService, DepartmentService departmentService) {
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
+        {
             _sellerService = sellerService;
             _departmentService = departmentService;
         }
-     
+
         public IActionResult Index()
         {
             var list = _sellerService.FindAll();
@@ -32,8 +33,30 @@ namespace SalesWebMVC.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Seller seller) {
+        public IActionResult Create(Seller seller)
+        {
             _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
