9727ae706b611b39fc8a1e8e5278b99414e03153
RacconWebFenix <dfenixweb@gmail.com>
2021-02-26 16:23:47 -0300

    Exception handling for delete (referential integrity)

--- SalesWebMVC/SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/SalesWebMVC/Services/SellerService.cs
@@ -35,9 +35,16 @@ namespace SalesWebMVC.Services
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException) 
+            {
+                throw new IntegrityException("Não foi possivel excuir vendedor. Ele(a) tem vendas cadastradas!");
+            }
         }
 
         public async Task UpdateAsync(Seller obj)
